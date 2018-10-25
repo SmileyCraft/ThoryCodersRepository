799631f9b87e11530f4dd786e9ecd23c5ada3aeb
DESKTOP-CRQRHD2\miked <mikedevries.smiley@gmail.com>
2018-10-25 22:59:36 +0200

    Sorted the cpp files into folders.

--- tcr/main.cpp
+++ tcr/main.cpp
@@ -1,30 +1,30 @@
 
 #define LOG
 
-#include "template.cpp"
-#include "geometryessentials.cpp"
-#include "prettyprinting.cpp"
+#include "template/template.cpp"
+#include "template/geometryessentials.cpp"
+#include "template/prettyprinting.cpp"
 
-#include "unionfind.cpp"
-#include "linkedlist.cpp"
-#include "trie.cpp"
-#include "segmenttree.cpp"
+#include "datastructures/unionfind.cpp"
+#include "datastructures/linkedlist.cpp"
+#include "datastructures/trie.cpp"
+#include "datastructures/segmenttree.cpp"
 
-#include "shoelace.cpp"
-#include "intersections.cpp"
-#include "convexhull.cpp"
+#include "geometry/shoelace.cpp"
+#include "geometry/intersections.cpp"
+#include "geometry/convexhull.cpp"
 
-#include "dijkstra.cpp"
-#include "bellmanford.cpp"
-#include "topologicalsort.cpp"
-#include "stronglyconnectedcomponents.cpp"
-#include "prim.cpp"
+#include "graphs/dijkstra.cpp"
+#include "graphs/bellmanford.cpp"
+#include "graphs/topologicalsort.cpp"
+#include "graphs/stronglyconnectedcomponents.cpp"
+#include "graphs/prim.cpp"
 
-#include "stringmatching.cpp"
-#include "longestincreasingsubsequence.cpp"
+#include "stringssequences/stringmatching.cpp"
+#include "stringssequences/longestincreasingsubsequence.cpp"
 
-#include "primes.cpp"
-#include "euclid.cpp"
+#include "maths/primes.cpp"
+#include "maths/euclid.cpp"
 
 int main(){
     cout << "-----o-----o----- DATA STRUCTURES -----o-----o-----" << endl << endl;
